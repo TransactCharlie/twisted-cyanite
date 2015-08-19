e6e79562b00298336cc01177f60b86f9283473ad
Charlie Gildawie <charlie.gildawie@skyscanner.net>
2015-08-19 15:35:14 +0100

    interface framwork for line and pickle input

---
+++ src/metrics_interface/protocols.py
@@ -0,0 +1,95 @@
+__author__ = 'charlie'
+
+from twisted.internet.protocol import Protocol
+from twisted.protocols.basic import LineReceiver
+from twisted.python import log
+from struct import unpack, calcsize
+import cPickle as pickle
+from twisted.internet.protocol import Factory
+
+
+class PickleProtocol(Protocol):
+
+    paused = False
+    structFormat = "!I"
+    prefixLength = calcsize(structFormat)
+    MAX_LENGTH = 99999
+    _unprocessed = b""
+    _compatibilityOffset = 0
+
+    def dataReceived(self, data):
+        """
+        Convert int prefixed strings into calls to stringReceived.
+        """
+        # Try to minimize string copying (via slices) by keeping one buffer
+        # containing all the data we have so far and a separate offset into that
+        # buffer.
+        alldata = self._unprocessed + data
+        currentOffset = 0
+        prefixLength = self.prefixLength
+        fmt = self.structFormat
+        self._unprocessed = alldata
+
+        while len(alldata) >= (currentOffset + prefixLength) and not self.paused:
+            messageStart = currentOffset + prefixLength
+            length, = unpack(fmt, alldata[currentOffset:messageStart])
+            if length > self.MAX_LENGTH:
+                self._unprocessed = alldata
+                self._compatibilityOffset = currentOffset
+                self.lengthLimitExceeded(length)
+                return
+            messageEnd = messageStart + length
+            if len(alldata) < messageEnd:
+                break
+
+            # Here we have to slice the working buffer so we can send just the
+            # netstring into the stringReceived callback.
+            packet = alldata[messageStart:messageEnd]
+            currentOffset = messageEnd
+            self._compatibilityOffset = currentOffset
+            self.stringReceived(packet)
+
+            # Check to see if the backwards compat "recvd" attribute got written
+            # to by application code.  If so, drop the current data buffer and
+            # switch to the new buffer given by that attribute's value.
+            if 'recvd' in self.__dict__:
+                alldata = self.__dict__.pop('recvd')
+                self._unprocessed = alldata
+                self._compatibilityOffset = currentOffset = 0
+                if alldata:
+                    continue
+                return
+
+        # Slice off all the data that has been processed, avoiding holding onto
+        # memory to store it, and update the compatibility attributes to reflect
+        # that change.
+        self._unprocessed = alldata[currentOffset:]
+        self._compatibilityOffset = 0
+
+    def stringReceived(self, data):
+        try:
+            unpickled = pickle.loads(data)
+
+            for d in unpickled:
+                log.msg(d)
+
+        except pickle.UnpicklingError:
+            log.err('invalid pickle received')
+
+
+
+class metrics_input_pickle_factory(Factory):
+    """Factory for collecting metrics from connections"""
+    protocol = PickleProtocol
+
+
+class LineProtocol(LineReceiver):
+
+    delimiter = "\n"
+
+    def lineReceived(self, line):
+        log.msg(line)
+
+class metrics_input_line_factory(Factory):
+    """Factory for collecting metrics from connections"""
+    protocol = LineProtocol
--- src/tx_cyanite.py
+++ src/tx_cyanite.py
@@ -4,7 +4,8 @@ import sys
 from twisted.web import server
 from twisted.internet import reactor
 from twisted.python import log
-from http_interface.protocol import endpoints as http_endpoints
+from http_interface.protocols import endpoints as http_endpoints
+from metrics_interface.protocols import metrics_input_pickle_factory, metrics_input_line_factory
 
 # Start the logger
 log.startLogging(sys.stdout)
@@ -15,5 +16,8 @@ if __name__ == "__main__":
     reactor.listenTCP(8080, server.Site(http_endpoints()), interface="0.0.0.0")
 
     # Register the Metrics TCP Interfaces
-    # TODO: Write a Metrics TCP Interface!
+    reactor.listenTCP(2003, metrics_input_line_factory())
+    reactor.listenTCP(2004, metrics_input_pickle_factory())
+
+    # Start
     reactor.run()
